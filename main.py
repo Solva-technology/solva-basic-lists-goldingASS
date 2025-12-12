6bca2e4e9db67b2eb80f74ee28317a0b07046f58
polyak33 <arpolpvl1@gmail.com>
2025-12-12 14:06:08 +0500

    задания по спискам

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ def get_first_and_last_element(lst: list) -> list:
     
     # TODO: Напишите ваш код здесь. 
     # Возьмите первый и последний элемент из `lst` и положите их в `result`.
-    
+    if len(lst) == 0:
+        return []
+    result = [lst[0], lst[-1]]
     # Этот код возвращает результат для проверки. Не меняйте его.
     return result
 
@@ -36,7 +38,7 @@ def get_middle_elements(lst: list) -> list:
     
     # TODO: Напишите ваш код здесь.
     # Используйте срезы, чтобы получить нужные элементы из `lst`.
-    
+    result=lst[1:-1]
     return result
 
 def get_elements_from_second_to_fourth(lst: list) -> list:
@@ -51,7 +53,7 @@ def get_elements_from_second_to_fourth(lst: list) -> list:
     result = []
     
     # TODO: Напишите ваш код здесь.
-    
+    result=lst[1:4]
     return result
 
 def add_element_to_start_and_end(lst: list, element) -> list:
@@ -67,7 +69,7 @@ def add_element_to_start_and_end(lst: list, element) -> list:
     
     # TODO: Напишите ваш код здесь.
     # Используйте методы для добавления элементов.
-    
+    lst=[element]+lst+[element]
     return lst
 
 def remove_second_element(lst: list) -> list:
@@ -79,7 +81,7 @@ def remove_second_element(lst: list) -> list:
     
     Пример: [1, 2, 3, 4] -> [1, 3, 4]
     """
-    
+    return lst[:1] + lst[2:]
     # TODO: Напишите ваш код здесь.
     
     return lst
